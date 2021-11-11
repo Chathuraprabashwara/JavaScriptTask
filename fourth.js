57c6980c7811924ba68ad832a36b8177f7fa68c6
WHO AM  I <60086327+Chathuraprabashwara@users.noreply.github.com>
2021-11-11 16:38:03 +0530

    add Fourth task assignment 9

---
+++ fourth.js
@@ -0,0 +1,8 @@
+function ChessboardTraveling(numx,numy,numa,numb){
+    var xDiffrence = numa- numx
+    var yDiffrence = numb - numy
+return (xDiffrence +yDiffrence)
+
+}
+   
+console.log(ChessboardTraveling(3,4,5,6))

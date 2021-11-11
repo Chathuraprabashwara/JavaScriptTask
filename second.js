5dcdc9b8fed1d45d910efcd8eb2d456197cc604b
WHO AM  I <60086327+Chathuraprabashwara@users.noreply.github.com>
2021-11-11 13:16:52 +0530

    add second task Assigment 7

---
+++ second.js
@@ -0,0 +1,15 @@
+
+
+function CenteredPentagonalNumber(num) {
+    
+    console.log(num)
+   dots = (5*(num*num) - 5*(num) + 2)/2
+    
+
+   
+    
+    return dots
+}
+
+console.log(CenteredPentagonalNumber("5"));
+console.log("chathura")
